a172a6d1fab26e3415843fbaae34f1fdb27815ae
VASS\mmolina <mmolina@serbatic.es>
2024-04-29 18:17:09 +0200

    Revisión de la estructura de datos

--- src/app/researching/research-form/extractor/extractors/spotify-extractor.ts
+++ src/app/researching/research-form/extractor/extractors/spotify-extractor.ts
@@ -19,9 +19,7 @@ export function extractSpotifyData(INPUT_CONTENT: any, INPUT_TYPE: string){
     return closingError('No se ha especificado el tipo del input');
   }
 
-  /* ---------------------------------------------------------------------------------------------- */
-  /* --- Extractor de JSON ------------------------------------------------------------------------ */
-  /* ---------------------------------------------------------------------------------------------- */
+
 
   const extractSpotifyJSON = (json: any) => {
 
@@ -37,7 +35,10 @@ export function extractSpotifyData(INPUT_CONTENT: any, INPUT_TYPE: string){
       return closingError('El JSON introducido no proviene de la página web oficial de Spotify');
     }
 
-    /* INFORMACIÓN DEL ARTISTA */
+    /* -------------------------------------------------------------------------------------------- */
+    /* --- INFORMACIÓN DEL ARTISTA ---------------------------------------------------------------- */
+    /* -------------------------------------------------------------------------------------------- */
+
     let artistOverview = json.log.entries
       /* Filtrar la petición de información del artista */
       .filter((entry: any) => entry.request.url.includes('query?operationName=queryArtistOverview') && entry.response.content.text)
@@ -50,10 +51,60 @@ export function extractSpotifyData(INPUT_CONTENT: any, INPUT_TYPE: string){
         delete data.preRelease;
         delete data.saved;
         delete data.__typename;
+        delete data.goods.events.userLocation;
+        delete data.profile.pinnedItem;
+        delete data.profile.verified;
+        data.goods.events.concerts = data.goods.events.concerts.items.map((x: any) => {
+          return { ...x, partnerLinks: x.partnerLinks.items, venue: { ...x.venue, location: x.venue.location.name } };
+        });
+        data.goods.merch = data.goods.merch.items.map((x: any) => ({ ...x, image: x.image.sources[0].url })); //Siempre sources[0]
+        data.profile.biography = data.profile.biography.text;
+        data.profile.externalLinks = data.profile.externalLinks.items;
+        data.profile.playlists = data.profile.playlistsV2.items.map((x: any) => { 
+          x = { ...x.data, image: x.data.images.items.map((y: any) => y.sources[0])[0], owner: x.data.ownerV2.data.name }; //Siempre sources[0]
+          delete x.ownerV2
+          delete x.images
+          delete x.__typename;
+          return x;
+        });
+        delete data.profile.playlistsV2;
+        data.relatedContent.appearsOn = data.relatedContent.appearsOn.items.map((x: any) => x.releases.items.map((y: any) => {
+          let artists = y.artists.items.map((z: any) => ({ name: z.profile.name, uri: z.uri }));
+          return { ...y, artists, coverArt: y.coverArt.sources[0] }; //Siempre sources[0]
+        })[0]);
+        data.relatedContent.discoveredOn = data.relatedContent.discoveredOnV2.items.map((x: any) => {
+          x = { ...x.data, image: x.data.images.items[0].sources[0], owner: x.data.ownerV2.data.name }; //Siempre sources[0]
+          delete x.ownerV2;
+          delete x.images
+          delete x.__typename;
+          return x;
+        });
+        delete data.relatedContent.discoveredOnV2;
+        data.relatedContent.featuring = data.relatedContent.featuringV2.items.map((x: any) => {
+          x = { ...x.data, image: x.data.images.items[0].sources[0], owner: x.data.ownerV2.data.name }; //Siempre sources[0]
+          delete x.ownerV2;
+          delete x.images
+          delete x.__typename;
+          return x;
+        });
+        delete data.relatedContent.featuringV2;
+        data.relatedContent.relatedArtists = data.relatedContent.relatedArtists.items.map((x: any) => {
+          x = { ...x, name: x.profile.name, visuals: x.visuals.avatarImage.sources }; //Nunca sources[0]
+          delete x.profile;
+          return x;
+        });
+        data.relatedVideos = data.relatedVideos.items;
+        data.stats.topCities = data.stats.topCities.items;
+        data.visuals.avatarImage.extractedColors = data.visuals.avatarImage.extractedColors.colorRaw.hex;
+        data.visuals.gallery = data.visuals.gallery.items.map((x: any) => x.sources[0]); //Siempre sources[0]
+        data.visuals.headerImage.extractedColors = data.visuals.headerImage.extractedColors.colorRaw.hex;
         return data;
       })[0];
     
-    /* LISTA DE ÁLBUMES */
+    /* -------------------------------------------------------------------------------------------- */
+    /* --- LISTA DE ÁLBUMES ----------------------------------------------------------------------- */
+    /* -------------------------------------------------------------------------------------------- */
+
     let discographyAlbums = json.log.entries
       /* Filtrar la petición de la discografía del artista */
       .filter((entry: any) => entry.request.url.includes('query?operationName=queryArtistDiscographyAll') && entry.response.content.text)
@@ -62,11 +113,20 @@ export function extractSpotifyData(INPUT_CONTENT: any, INPUT_TYPE: string){
       /* Obtener listado de álbumes  */
       .map((artist: any) => artist.data.artistUnion.discography.all.items
         .map((album: any) => album.releases.items[0])
-        .map((album: any) => ({ ...album, tracks: [] }))
+        .map((album: any) => {
+          album = { ...album, tracks: [], coverArt: album.coverArt.sources };
+          delete album.playability;
+          return album;
+        })
+      )[0]
+
       /* Ordenar álbumes de forma cronológica */
-      )[0].sort((a: any, b: any) => a.date.isoString.localeCompare(b.date.isoString));
+      .sort((a: any, b: any) => a.date.isoString.localeCompare(b.date.isoString));
       
-    /* LISTA DE CANCIONES */
+    /* -------------------------------------------------------------------------------------------- */
+    /* --- LISTA DE CANCIONES --------------------------------------------------------------------- */
+    /* -------------------------------------------------------------------------------------------- */
+
     let topTracks = json.log.entries
       /* Filtrar las peticiones de canciones */
       .filter((entry: any) => entry.request.url.includes('query?operationName=queryAlbumTracks') && entry.response.content.text)
@@ -76,11 +136,24 @@ export function extractSpotifyData(INPUT_CONTENT: any, INPUT_TYPE: string){
         entry = JSON.parse(entry.response.content.text).data.albumUnion.tracks.items.map((song: any) => song.track);
         discographyAlbums.forEach((album: any) => {
           if(entryUrl.includes(album.id)){
+            /* Añadir álbumes sin canciones a las canciones de la lista de canciones */
             entry.forEach((song: any) => {
               song.album = { ...album, tracks: [] };
               return song;
             });
-            album.tracks = [ ...album.tracks, ...entry ];
+            /* Añadir canciones sin álbum a los álbumes de la lista de álbumes */
+            album.tracks = entry.map((song: any) => {
+              song = { ...song, artists: song.artists.items, associatedVideos: song.associations.associatedVideos, 
+                playcount: parseInt(song.playcount), duration: song.duration.totalMilliseconds };
+              delete song.album;
+              delete song.associations;
+              delete song.discNumber;
+              delete song.relinkingInformation;
+              delete song.saved;
+              delete song.playability;
+              delete song.contentRating;
+              return song;
+            });
           }
           return album;
         });
@@ -88,6 +161,18 @@ export function extractSpotifyData(INPUT_CONTENT: any, INPUT_TYPE: string){
       })
       /* Obtener listado de canciones */
       .reduce((res: any[], album: any) => res = [ ...res, ...album ], [])
+      /* Modificar estructura de las canciones */
+      .map((song: any) => {
+        song = { ...song, artists: song.artists.items, associatedVideos: song.associations.associatedVideos, 
+          playcount: parseInt(song.playcount), duration: song.duration.totalMilliseconds };
+        delete song.associations;
+        delete song.discNumber;
+        delete song.relinkingInformation;
+        delete song.saved;
+        delete song.playability;
+        delete song.contentRating;
+        return song;
+      })
       /* Ordenar por número de reproducciones */
       .sort((a: any, b: any) => parseInt(b.playcount) - parseInt(a.playcount))
       /* Agrupar duplicados en grupos */
@@ -103,21 +188,26 @@ export function extractSpotifyData(INPUT_CONTENT: any, INPUT_TYPE: string){
         if(group.length > 1){
           group = group.sort((a: any, b: any) => a.album.date.isoString.localeCompare(b.album.date.isoString));
           let mainSong!: any;
+          let sortName = group.sort((a: any, b: any) => a.name.length - b.name.length)[0].name;
           if(group[0].album.type === 'ALBUM' || !group.some((a: any) => a.album.type === 'ALBUM')){
             mainSong = group.shift();
             mainSong.otherVersions = group;
           } else {
-            mainSong = group.find((a: any) => a.album.type === 'ALBUM'); //TODO Encontrar la canción con el título más corto y que pertenezca a un álbum tipo ALBUM
+            mainSong = group.find((a: any) => a.album.type === 'ALBUM');
             mainSong.otherVersions = group.filter((a: any) => a.album.id !== mainSong.album.id);
           }
+          mainSong.originalName = sortName;
           return mainSong;
         }
         return group[0];
       });
     
-    /* Añadir álbum a las canciones del Top 10 */
+    /* -------------------------------------------------------------------------------------------- */
+    /* --- TOP 10 DE CANCIONES -------------------------------------------------------------------- */
+    /* -------------------------------------------------------------------------------------------- */
+
     artistOverview.topTracks.forEach((topSong: any) => {
-      let album = topTracks.reduce((res: any, song: any) => {
+      let album = topTracks.reduce((res: any, song: any) => { //TODO Arreglar álbumes vacíos
         if(topSong.playcount === song.playcount && 
           (song.name.toLowerCase().includes(topSong.name.toLowerCase()) || topSong.name.toLowerCase().includes(song.name.toLowerCase()))){
           res = song.album;
