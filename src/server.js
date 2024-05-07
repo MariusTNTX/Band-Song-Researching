243cdfb6dbe1dda4ecb9b828263734456413921e
VASS\mmolina <mmolina@serbatic.es>
2024-05-07 17:53:42 +0200

    extractor de youtube music en versión alfa

--- src/server.js
+++ src/server.js
@@ -1,36 +1,52 @@
 import http from 'http';
 import { exec } from 'child_process';
 
-// Crea un servidor HTTP
+function getURLObject(url){
+  let endpoint = url.includes('?') ? url.split('?')?.[0].replace('http://localhost:3000', '') || null : url;
+  let params = url.includes('?') ? url.split('?')?.[1] || null : null;
+  params = params ? params.split('&').reduce((res, param) => ({ ...res, [param.split('=')[0]]: param.split('=')[1] }), {}) : null;
+  return { url, endpoint, params };
+}
+
+// HTTP Server
 const server = http.createServer((req, res) => {
-  if (req.url === '/puppeteer' && req.method === 'GET') {
-    // Ejecuta tu script de Puppeteer
-    exec('node src/app/researching/research-form/extractor/puppeteer/index.js --url https://music.youtube.com/channel/UCnDibivBAgWYKWi9qOiCNnA', (error, stdout, stderr) => {
+  const request = getURLObject(req.url);
+  /* ---------------------------------------------------------------------------------------------- */
+  /* /PUPPETEER/YOUTUBEMUSIC -----------------------------------------------------------------------*/
+  /* ---------------------------------------------------------------------------------------------- */
+  if (request.endpoint === '/puppeteer/youtubemusic' && req.method === 'GET') {
+    var args = '';
+    if(request.params.url){
+      args = `--url ${request.params.url}`;
+    } else if(request.params.id){
+      args = `--url https://music.youtube.com/channel/${request.params.id}`;
+    } else if(request.params.band){
+      args = `--band ${request.params.band}`;
+    } else {
+      res.statusCode = 404;
+      res.end(JSON.stringify({ error: 'Parámetros no válidos', request }));
+    }
+    exec(`node src/app/researching/research-form/extractor/puppeteer/index.js ${args}`, (error, stdout, stderr) => {
       if (error) {
-        console.error(`Error al ejecutar el script de Puppeteer: ${error.message}`);
+        console.error(`Error al ejecutar el script: ${error.message}`);
         res.statusCode = 500;
-        res.end(JSON.stringify({ error: 'Error al ejecutar el script de Puppeteer: ' + error }));
+        res.end(JSON.stringify({ error: 'Error al ejecutar el script: ' + error }));
         return;
       }
       if (stderr) {
-        console.error(`Error de salida estándar del script de Puppeteer: ${stderr}`);
+        console.error(`Error de salida estándar del script: ${stderr}`);
       }
-
-      // Define el encabezado de la respuesta como JSON
       res.setHeader('Content-Type', 'application/json');
-
-      // Define el código de estado y el cuerpo de la respuesta
       res.statusCode = 200;
-      res.end(stdout); // Devuelve la salida del script de Puppeteer como JSON
+      res.end(stdout);
     });
   } else {
-    // Si se recibe una solicitud en una ruta diferente a /puppeteer, devuelve un error 404
     res.statusCode = 404;
-    res.end(JSON.stringify({ error: 'Ruta no encontrada' }));
+    res.end(JSON.stringify({ error: 'Ruta no encontrada', request }));
   }
 });
 
-// Escucha en el puerto 3000
+// Port
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor Node.js funcionando en el puerto ${PORT}`);
